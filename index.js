5ffa397f661f7a28c904b67e13786350706718f2
Aedx91 <124948231+Aedx91@users.noreply.github.com>
2023-02-09 20:19:28 -0600

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-// Import stylesheets
-import './style.css';
+let inventory = {
+  items: [
+    { name: "apple", quantity: 5 },
+    { name: "banana", quantity: 2 },
+    { name: "orange", quantity: 3 }
+  ],
+  addItem: function(name, quantity) {
+    this.items.push({ name: name, quantity: quantity });
+  },
+  removeItem: function(name) {
+    let itemIndex = this.items.findIndex(item => item.name === name);
+    if (itemIndex !== -1) {
+      this.items.splice(itemIndex, 1);
+    }
+  },
+  updateItem: function(name, quantity) {
+    let item = this.items.find(item => item.name === name);
+    if (item) {
+      item.quantity = quantity;
+    }
+  }
+};
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// Example usage
+inventory.addItem("grapes", 4);
+inventory.removeItem("banana");
+inventory.updateItem("apple", 10);
+console.log(inventory.items);
